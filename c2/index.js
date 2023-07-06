592cb92141c85ee4d20335cb1a6b4fb8c6799ade
Vangel22 <h.vangel22@gmail.com>
2023-07-06 21:11:38 +0200

    Save with tasks

---
+++ c2/index.js
@@ -0,0 +1,31 @@
+if (true && true) {
+  //mora dvata operatori da bidat tocni
+}
+// true and true - returns true
+// false and true - returns false
+// true and false - returns false
+// false and false - returns false
+
+if (true || true) {
+  //samo eden od operatorite da bide tocno
+}
+
+// true and true - returns true
+// false and true - returns true
+// true and false - returns true
+// false and false - returns false
+
+if (false && false) {
+} //nor operacija
+
+if (true) {
+  //here
+} else {
+  //it is false
+}
+
+//   if (ova) {
+//     //izvrsi
+//   } else {
+//     //ova ne e tocno izvrsi tuka
+//   }
